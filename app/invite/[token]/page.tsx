5c924644927f749dbbb9fe6ad7d662520ceddfc1
mont3ll <meluseno@gmail.com>
2026-05-27 05:49:35 +0300

    feat(invite): add missing invite token pages and tests to redesign

---
+++ app/invite/[token]/page.tsx
@@ -0,0 +1,5 @@
+import SourceAdminPreview from "@/components/dashboard2/admin/source-admin-preview";
+
+export default function InviteTokenPage() {
+  return <SourceAdminPreview initialRoute="/sign-up?invite=demo-token&role=artisan" />;
+}
